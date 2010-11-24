4f320ef56d2cf0d1a1ff07009e9e710af70729cd
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2010-11-24 20:51:59 +0100

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordpressVersionChecker::Application.config.secret_token = '096ffe6315cd8bde960e2be73c813e9aaad53881b05848e930f41f23fd75f1e01afffc6974acb0ff0a258cfde52b4636bbd9c8d17bce05aaea30ed22d3059ecc'
